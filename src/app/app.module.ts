cd1e2d4149e4932c4034a8b884f91b4f8c69e3f7
Rodrigo <rodrigo.salinas23@hotmail.com>
2022-04-19 22:42:57 -0400

    Login listo, falta rutear las demas pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { SoporteComponent } from './components/soporte/soporte.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ClienteComponent,
+    SoporteComponent
   ],
   imports: [
     BrowserModule
