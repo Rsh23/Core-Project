9d9a27cde276c123cc43e4e5b44894a06bc3e0d8
Rodrigo <rodrigo.salinas23@hotmail.com>
2022-04-21 01:44:15 -0400

    Conectando components y linkeando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { PerfilComponent } from './shared/perfil/perfil.component';
-import { ClienteComponent } from './components/cliente/cliente.component';
-import { SoporteComponent } from './components/soporte/soporte.component';
-
-
+import { LoginComponent } from './components/pages/login/login.component';
+import { PerfilComponent } from './components/pages/perfil/perfil.component';
+import { ClienteComponent } from './components/pages/cliente/cliente.component';
+import { SoporteComponent } from './components/pages/soporte/soporte.component';
+import { BugsComponent } from './components/pages/bugs/bugs.component';
+import { DesarrolloComponent } from './components/pages/desarrollo/desarrollo.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { NotificacionesComponent } from './components/pages/notificaciones/notificaciones.component';
+import { ProyectosComponent } from './components/pages/proyectos/proyectos.component';
+import { ServiciosComponent } from './components/pages/servicios/servicios.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { RegisterComponent } from './components/pages/register/register.component';
 
 
 
@@ -14,17 +20,25 @@ const app_routes: Routes = [
     { path: 'perfil', component: PerfilComponent },
     { path: 'cliente', component: ClienteComponent },
     { path: 'soporte', component: SoporteComponent },
+    { path: 'bugs', component: BugsComponent },
+    { path: 'desarrollo', component: DesarrolloComponent },
+    { path: 'menu', component: MenuComponent },
+    { path: 'notificaciones', component: NotificacionesComponent },
+    { path: 'proyectos', component: ProyectosComponent },
+    { path: 'servicios', component: ServiciosComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'register', component: RegisterComponent },
     { path: '**', pathMatch: 'full', redirectTo: ''}
 
 ];
 
 @NgModule({
-    // imports: [
-    //     RouterModule.forRoot( app_routes )
-    // ],
-    // exports: [
-    //     RouterModule
-    // ]
+    imports: [
+        RouterModule.forRoot( app_routes )
+    ],
+    exports: [
+        RouterModule
+    ]
 
 })
 export class AppRoutingModule {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { ClienteComponent } from './components/cliente/cliente.component';
-import { SoporteComponent } from './components/soporte/soporte.component';
-import { PerfilComponent } from './shared/perfil/perfil.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { ClienteComponent } from './components/pages/cliente/cliente.component';
+import { SoporteComponent } from './components/pages/soporte/soporte.component';
+import { PerfilComponent } from './components/pages/perfil/perfil.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ProyectosComponent } from './components/pages/proyectos/proyectos.component';
+import { DesarrolloComponent } from './components/pages/desarrollo/desarrollo.component';
+import { ServiciosComponent } from './components/pages/servicios/servicios.component';
+import { NotificacionesComponent } from './components/pages/notificaciones/notificaciones.component';
+import { BugsComponent } from './components/pages/bugs/bugs.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +23,16 @@ import { AppRoutingModule } from './app-routing.module';
     LoginComponent,
     ClienteComponent,
     SoporteComponent,
-    PerfilComponent
+    PerfilComponent,
+    HomeComponent,
+    ProyectosComponent,
+    DesarrolloComponent,
+    ServiciosComponent,
+    NotificacionesComponent,
+    BugsComponent,
+    MenuComponent,
+    RegisterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
