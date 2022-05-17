5500e8029417ca9d954fd7bbe8ced12eb74ce68f
Rodrigo Salinas <rodrigo.salinas23@hotmail.com>
2022-05-17 17:12:50 -0400

    Modificando el chat, agregando funciones

--- src/app/components/pages/general/chat/chat.component.ts
+++ src/app/components/pages/general/chat/chat.component.ts
@@ -9,7 +9,61 @@ export class ChatComponent implements OnInit {
 
   constructor() { }
 
+  divChatChat: boolean = false;
+  divChatGrupos: boolean = true;
+  divChatGrupos2: boolean = true;
+  divChatPersonas: boolean = false;
+  divChatChatMensaje: boolean = false;
+
   ngOnInit(): void {
+  }
+
+  hideChatGrupos (){
+
+    this.divChatChat = false;
+    this.divChatPersonas = false;
+    this.divChatGrupos = true;
+  
+  }
+
+  showChatChat( numChat: number ){
+
+    this.divChatChatMensaje = true;
+    this.divChatChat = true;
+    this.divChatPersonas = false;
+
+  }
+
+  hideChatChat( numGrupo: number ){
+
+    this.divChatPersonas = true;
+    this.divChatChat = false;
+    this.divChatChatMensaje = false;
+
+  }
+
+  showChatPersonas( numChat: number ){
+
+    if( numChat == 1 ){
+
+      this.divChatChat = false;
+      this.divChatPersonas = true;
+      this.divChatGrupos = false;
+      this.divChatGrupos2 = false;
+
+    }
+    else{
+
+      alert("Error");
+
+    }
+
+  }
+
+  enviarMensaje(){
+
+    // document.getElementById("divChat").innerHTML += "<h3>"+ ("#inputchat").val()+"</h3></br></br>";
+
   }
 
 }
