86e2b119fabd03d5bdb14e131981095be8931bdc
Rodrigo Salinas <rodrigo.salinas23@hotmail.com>
2022-06-01 16:43:25 -0400

    Agregando ng-bootstrap y creando modals de la seccion soporte

--- src/app/components/pages/cliente/soporte-cs/soporte-cs.component.ts
+++ src/app/components/pages/cliente/soporte-cs/soporte-cs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-soporte-cs',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SoporteCSComponent implements OnInit {
 
-  constructor() { }
+  constructor( public modal: NgbModal ) { }
 
   ngOnInit(): void {
+  }
+
+  // Esto abre el modal para crear un ticket
+  openSM(crearTicket: any){
+    this.modal.open(crearTicket, {centered: true});
+  }
+
+  // Esto abre el modal para actualizar un ticket
+  openUT(updateTicket: any){
+    this.modal.open(updateTicket, {centered: true});
+  }
+
+  openLT(listarTicket: any){
+    this.modal.open(listarTicket, {centered: true});
+  }
+
+  openDOC(documentacion: any){
+    this.modal.open(documentacion, {centered: true});
   }
 
 }
